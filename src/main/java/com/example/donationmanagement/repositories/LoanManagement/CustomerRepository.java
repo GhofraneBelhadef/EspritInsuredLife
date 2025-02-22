90c5aece55cb53eceefbd4fc62a296830e7d472f
rouabr <roua.benromdhane@esprit.tn>
2025-02-22 17:00:30 +0100

    commit2

--- src/main/java/com/example/donationmanagement/repositories/LoanManagement/CustomerRepository.java
+++ src/main/java/com/example/donationmanagement/repositories/LoanManagement/CustomerRepository.java
@@ -1,9 +1,9 @@
 package com.example.donationmanagement.repositories.LoanManagement;
 
-import com.example.donationmanagement.entities.LoanManagement.RiskBreakdown;
+import com.example.donationmanagement.entities.LoanManagement.Customer; // Fixed import
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RiskBreakdownRepository extends JpaRepository<RiskBreakdown, Long> {
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
 }
