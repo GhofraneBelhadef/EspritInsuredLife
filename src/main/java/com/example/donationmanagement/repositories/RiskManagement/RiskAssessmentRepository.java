d14018f1424e2e192927cd3a0638a1b871a19c0d
ghofrane1111 <ghofrane.belhadef@esprit.tn>
2025-03-06 15:12:47 +0100

    changement

--- src/main/java/com/example/donationmanagement/repositories/RiskManagement/RiskAssessmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.donationmanagement.repositories.RiskManagement;
-
-import com.example.donationmanagement.entities.RiskManagement.RiskAssessment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RiskAssessmentRepository extends JpaRepository<RiskAssessment, Long> {
-}
--- src/main/java/com/example/donationmanagement/repositories/RiskManagement/RiskFactorsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.donationmanagement.repositories.RiskManagement;
-
-import com.example.donationmanagement.entities.RiskManagement.RiskFactors;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface RiskFactorsRepository extends JpaRepository<RiskFactors, Long> {
-}
